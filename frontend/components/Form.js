eecd1f1c570d07bb908275396c39aa6eced94c34
cesar.aguilar <Blueavian9@gmail.com>
2022-08-08 19:38:34 -0700

    initial

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,35 @@
 import React from 'react'
-
+s
 export default class Form extends React.Component {
+  onSubmit = evt => {
+    evt.preventDefault()
+    this.props.onSubmit()
+  }
+
+  onChange = evt => {
+    const { value, id } = evt.target;
+    this.props.onChange{id, value}
+  }
+    
   render() {
     return (
-      <div>
-        Form
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          id="textInput"
+          placeholder="type text"
+          value={this.props.textInput}
+          onChange={this.onChange}
+        />
+        <input
+          type="text"
+          id="textInput"
+          placeholder="type text"App.js
+          value={this.props.textInput}
+          onChange={this.onChange}
+        />
+        <input type="submit" />
+      </form>
     )
   }
 }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -3,9 +3,36 @@ import React from 'react'
 export default class Todo extends React.Component {
   render() {
     return (
-      <div>
-        Todo
+      <div className="todo-list">
+        <h1>Todo {this.props.name}</h1>
+        <ul>
+          <li>Eat</li>
+          <li>Program</li>
+          <li>Sleep</li>
+          <li>Repeat</li>
+          <li>Create</li>
+          <li>Read</li>
+          <li>Upload</li>
+          <li>Delete</li>
+        </ul>
       </div>
     )
   }
 }
+
+// Example usage: <ShoppingList name="Cesar" />
+
+    return React.createElement('div', {className: 'todo-list'},
+      React.createElement('h1', null, this.props.name),
+      React.createElement('ul', null, this.props.name),
+      React.createElement('li', null, this.props.name),
+      React.createElement('li', null, this.props.name),
+      React.createElement('li', null, this.props.name),
+      React.createElement('li', null, this.props.name),
+      React.createElement('li', null, this.props.name),
+      React.createElement('li', null, this.props.name)
+    )
+   )
+  )
+ )
+);
